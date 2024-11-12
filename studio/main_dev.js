80173114c3650a17410759d3ea7a91e152c2df51
Antoine Richard <antoine.richard@nrco.fr>
2024-11-12 16:36:18 +0100

    Update keycloak connector

--- studio/main_dev.js
+++ studio/main_dev.js
@@ -1,55 +1,55 @@
-import Studio from 'jsreport-studio'
-import Keycloak from 'keycloak-js'
+import Studio from "jsreport-studio";
+import Keycloak from "keycloak-js";
 
 const hasRole = (keycloak) => {
-  let resource
-  let role = keycloak._config.role
+  let resource;
+  let role = keycloak._config.role;
 
-  if (role.indexOf(':') >= 0) {
-    [ resource, role ] = role.split(':')
+  if (role.indexOf(":") >= 0) {
+    [resource, role] = role.split(":");
   }
 
   if (resource) {
-    return keycloak.hasResourceRole(role, resource)
+    return keycloak.hasResourceRole(role, resource);
   } else {
-    return keycloak.hasResourceRole(role)
+    return keycloak.hasResourceRole(role);
   }
-}
+};
 
 const checkAuth = (keycloak) => {
   return new Promise((resolve, reject) => {
     if (!keycloak.token) {
-      keycloak.init({ onLoad: 'login-required' }).success(() => {
+      keycloak.init({ onLoad: "login-required" }).then(() => {
         if (!hasRole(keycloak)) {
-          window.location = keycloak.createLogoutUrl()
+          window.location = keycloak.createLogoutUrl();
         } else {
-          resolve(keycloak.token)
+          resolve(keycloak.token);
         }
-      })
+      });
     } else {
-      keycloak.updateToken(5).success(() => {
+      keycloak.updateToken(5).then(() => {
         if (!hasRole(keycloak)) {
-          window.location = keycloak.createLogoutUrl()
+          window.location = keycloak.createLogoutUrl();
         } else {
-          resolve(keycloak.token)
+          resolve(keycloak.token);
         }
-      })
+      });
     }
-  })
-}
+  });
+};
 
 Studio.initializeListeners.unshift(async () => {
-  let keycloakConfig = await Studio.API.get('/auth/config')
-  let keycloak = new Keycloak(keycloakConfig)
-  keycloak._config = keycloakConfig
-  let token = await checkAuth(keycloak)
-  Studio.setRequestHeader('Authorization', 'Bearer ' + token)
+  let keycloakConfig = await Studio.API.get("/auth/config");
+  let keycloak = new Keycloak(keycloakConfig);
+  keycloak._config = keycloakConfig;
+  let token = await checkAuth(keycloak);
+  Studio.setRequestHeader("Authorization", "Bearer " + token);
   for (let name in Studio.API) {
-    const ex = Studio.API[name]
+    const ex = Studio.API[name];
     Studio.API[name] = async (...args) => {
-      let token = await checkAuth(keycloak, keycloakConfig.role)
-      Studio.setRequestHeader('Authorization', 'Bearer ' + token)
-      return ex(...args)
-    }
+      let token = await checkAuth(keycloak, keycloakConfig.role);
+      Studio.setRequestHeader("Authorization", "Bearer " + token);
+      return ex(...args);
+    };
   }
-})
+});
